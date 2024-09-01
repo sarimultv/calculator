8e27380b5f9bc806f08d805455276af1039bdd4b
Sarimul Hoque Laskar <49907699+sarimultv@users.noreply.github.com>
2024-09-01 17:08:48 +0530

    Delete todolist directory
    other web app

--- todolist/src/components/ErrorMessage.jsx
+++
@@ -1,7 +0,0 @@
-import "./ErrorMessage.module.css";
-
-const ErrorMessage = () => {
-  return <p>To-Do items not available, Chill...!!!</p>;
-};
-
-export default ErrorMessage;
--- todolist/src/components/Header.jsx
+++
@@ -1,6 +0,0 @@
-import styles from "./Header.module.css";
-
-const Header = () => {
-  return <h2 className={styles.heading}>To-Do List</h2>;
-};
-export default Header;
